18fda32e32d42dac560ff2056b29386d8c20e083
MaxHuber88 <55286559+MaxHuber88@users.noreply.github.com>
2019-09-30 11:01:26 -0400

    Update Pig Latin.py

--- Pig Latin.py
+++ Pig Latin.py
@@ -1,13 +1,17 @@
 # by Max Huber
+# init variables
 word = input("Enter word: ")
 vowels = ['a','e','i','o','u','y']
 isVowelCase = False
 i = 0
+
+# check if starts with vowel
 for x in vowels:
 	if word[:1] == vowels[i]:
 		isVowelCase = True
 	i = i + 1
-		
+
+#print
 if isVowelCase == True:
 	print(word+"ay")
 else:
